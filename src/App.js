71c44035e79f619657c2d811e673a0a4ccdd3f46
Mazen Mohamed <mazenmezomohammed@gmail.com>
2024-01-16 22:10:51 +0200

    Deleted create-react-app extras, added basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   fetch(
+  //     "api.openweathermap.org/data/2.5/forecast?q=cairo&appid=d0567773a9175ee190f9d62effeaf55a"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => setData(json))
+  //     .catch((error) => console.error(error));
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."} */}
+      <div class="grid-container">
+        <input placeholder="Search for cities" class="section1"></input>
+        <div class="section2">
+          <div>asdfsgvasdf</div>
+          <div>asdfsgvasdf</div>
+          <div>asdfsgvasdf</div>
+        </div>
+        <div class="section3">
+          <h2>Cairo</h2>
+          <h1>31C</h1>
+        </div>
+        <div class="section4">
+          <div>TODAY'S FORECAST</div>
+          <div>25</div>
+          <div>25</div>
+          <div>25</div>
+          <div>25</div>
+          <div>25</div>
+          <div>25</div>
+        </div>
+        <div class="section5">
+          <div>asdfsgvasdf</div>
+          <div>asdfsgvasdf</div>
+          <div>asdfsgvasdf</div>
+        </div>
+      </div>
     </div>
   );
 }
